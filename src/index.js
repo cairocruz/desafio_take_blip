f0bd25a3fdcac87ffe19350452d103f1c9dd59fc
cairocruz <cruzcairo@gmail.com>
2021-10-27 14:08:20 -0300

    verify

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const cors = require('cors')
 
 const app = express();
 
-const port = process.env.PORT || 8085;
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 app.use(require('./routes'))
 
-app.listen(port, function(){
-    console.log("server on port:" + port)
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
